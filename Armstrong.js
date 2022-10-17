b3fb97788d7d62e44b5d39458f47206f32e3b052
vansh-kashyap <nootyvansh2@gmail.com>
2022-10-17 18:16:07 +0530

    armstrong number

---
+++ Armstrong.js
@@ -0,0 +1,19 @@
+
+
+  const arm = (num)=>{
+      let string =num+"";
+      let array = string.split("").map((data)=>{
+          return parseInt(data)
+      });
+      const number= array.map((data)=>{
+          return Math.pow(data,array.length)
+      })
+     const finalOutput = (number.reduce((a,b)=> a+b ))
+     if(finalOutput==num){
+         console.log("Armstrong number")
+     }
+     else{
+         console.log("Not an armstring number")
+      }
+  }
+  arm(156);
