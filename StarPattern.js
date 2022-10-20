e94efd88daaadfd1fb21d58d177b4dc60eb3baa8
vansh-kashyap <nootyvansh2@gmail.com>
2022-10-20 12:03:56 +0530

    commit

---
+++ StarPattern.js
@@ -0,0 +1,22 @@
+let str = "";
+
+
+for(i=0;i<=5;i++){
+  for(j=0;j<i;j++){
+    str +="*"
+  }
+   str +='\n'; 
+}
+console.log(str);
+
+
+/*   The output will be 
+
+
+*
+**
+***
+****
+*****
+
+*/
